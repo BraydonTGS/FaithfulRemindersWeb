3613d2253b49d344332071015d988c6767ff79a0
Braydon Sutherland <99298723+BraydonTGS@users.noreply.github.com>
2024-01-23 06:01:15 -0600

    Created BaseRepository and Interface, Not Implemented

---
+++ FaithfulRemindersWeb.Business/Interfaces/IBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using FaithfulRemindersWeb.Entity.Entities.Base;
+
+namespace FaithfulRemindersWeb.Business.Interfaces
+{
+    internal interface IBaseRepository<TEntity> where TEntity : BaseEntity
+    {
+        public Task<IEnumerable<TEntity>?> GetAllAsync();
+        public Task<TEntity?> GetByIdAsync(object id);
+        public Task<TEntity?> CreateAsync(TEntity entity);
+        public Task<TEntity?> UpdateAsync(TEntity entity);
+        public Task<bool> SoftDeleteAsync(object id);
+        public Task<bool> HardDeleteAsync(object id);
+    }
+}
---
+++ FaithfulRemindersWeb.Business/Repository/Base/BaseRepository.cs
@@ -0,0 +1,38 @@
+﻿using FaithfulRemindersWeb.Business.Interfaces;
+using FaithfulRemindersWeb.Entity.Entities.Base;
+
+namespace FaithfulRemindersWeb.Business.Repository.Base
+{
+    internal class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : BaseEntity
+    {
+        public Task<TEntity?> CreateAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TEntity>?> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity?> GetByIdAsync(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> HardDeleteAsync(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SoftDeleteAsync(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity?> UpdateAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
