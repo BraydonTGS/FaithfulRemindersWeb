2260e394d5d8e2ae7fbab933c048e239fc7e271e
Braydon Sutherland <99298723+BraydonTGS@users.noreply.github.com>
2024-01-23 18:11:36 -0600

    Implemented BaseRepository Methods, Added XML Summaries

--- FaithfulRemindersWeb.Business/Interfaces/IBaseRepository.cs
+++ FaithfulRemindersWeb.Business/Interfaces/IBaseRepository.cs
@@ -2,6 +2,11 @@
 
 namespace FaithfulRemindersWeb.Business.Interfaces
 {
+    /// <summary>
+    /// Generic interface defining basic CRUD (Create, Read, Update, Delete) operations for entities.
+    /// </summary>
+    /// <typeparam name="TEntity">The type of entity managed by the repository.</typeparam>
+    /// <typeparam name="TKey">The type of the entity's primary key.</typeparam>
     internal interface IBaseRepository<TEntity, TKey> where TEntity : BaseEntity<TKey>
     {
         public Task<IEnumerable<TEntity>?> GetAllAsync();
@@ -9,6 +14,7 @@ namespace FaithfulRemindersWeb.Business.Interfaces
         public Task<TEntity?> CreateAsync(TEntity entity);
         public Task<TEntity?> UpdateAsync(TEntity entity);
         public Task<bool> SoftDeleteAsync(TKey id);
+        public Task<bool> RestoreAsync(TKey id);
         public Task<bool> HardDeleteAsync(TKey id);
     }
 }
--- FaithfulRemindersWeb.Business/Repository/Base/BaseRepository.cs
+++ FaithfulRemindersWeb.Business/Repository/Base/BaseRepository.cs
@@ -1,43 +1,151 @@
-﻿using FaithfulRemindersWeb.Business.Interfaces;
+﻿using FaithfulRemindersWeb.Business.Context;
+using FaithfulRemindersWeb.Business.Interfaces;
 using FaithfulRemindersWeb.Entity.Entities.Base;
+using Microsoft.EntityFrameworkCore;
 
 namespace FaithfulRemindersWeb.Business.Repository.Base
 {
     /// <summary>
-    /// Base Repository for Basic CRUD Functionality
+    /// Generic repository providing basic CRUD (Create, Read, Update, Delete) functionality for entities.
     /// </summary>
-    /// <typeparam name="TEntity"></typeparam>
-    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TEntity">The type of entity managed by the repository.</typeparam>
+    /// <typeparam name="TKey">The type of the entity's primary key.</typeparam>
+    /// <see cref="IBaseRepository{TEntity, TKey}"/>
     internal class BaseRepository<TEntity, TKey> : IBaseRepository<TEntity, TKey> where TEntity : BaseEntity<TKey>
     {
-        public Task<TEntity?> CreateAsync(TEntity entity)
+        #region Private
+        private readonly IDbContextFactory<FaithfulDbContext> _contextFactory;
+        #endregion
+
+        public BaseRepository(IDbContextFactory<FaithfulDbContext> contextFactory)
         {
-            throw new NotImplementedException();
+            _contextFactory = contextFactory ?? throw new ArgumentNullException(nameof(contextFactory));
         }
 
-        public Task<IEnumerable<TEntity>?> GetAllAsync()
+        #region GetAllAsync
+        /// <summary>
+        /// Retrieves all entities of the specified type asynchronously.
+        /// </summary>
+        /// <returns>A collection of entities or null if none are found.</returns>
+        public async Task<IEnumerable<TEntity>?> GetAllAsync()
         {
-            throw new NotImplementedException();
-        }
+            using var context = _contextFactory.CreateDbContext();
 
-        public Task<TEntity?> GetByIdAsync(TKey id)
-        {
-            throw new NotImplementedException();
+            return await context.Set<TEntity>().ToListAsync();
         }
+        #endregion
 
-        public Task<bool> HardDeleteAsync(TKey id)
+        #region GetByIdAsync
+        /// <summary>
+        /// Retrieves an entity of the specified type by its unique identifier asynchronously.
+        /// </summary>
+        /// <param name="id">The unique identifier of the entity to retrieve.</param>
+        /// <returns>The retrieved entity or null if not found.</returns>
+        public async Task<TEntity?> GetByIdAsync(TKey id)
         {
-            throw new NotImplementedException();
-        }
+            using var context = _contextFactory.CreateDbContext();
 
-        public Task<bool> SoftDeleteAsync(TKey id)
-        {
-            throw new NotImplementedException();
+            return await context.Set<TEntity>().FindAsync(id);
         }
+        #endregion
 
-        public Task<TEntity?> UpdateAsync(TEntity entity)
+        #region CreateAsync
+        /// <summary>
+        /// Creates a new entity asynchronously and adds it to the database.
+        /// </summary>
+        /// <param name="entity">The entity to be created.</param>
+        /// <returns>The created entity after it is added to the database.</returns>
+        public async Task<TEntity?> CreateAsync(TEntity entity)
         {
-            throw new NotImplementedException();
+            using var context = _contextFactory.CreateDbContext();
+
+            var newEntry =  await context.Set<TEntity>().AddAsync(entity);
+
+            await context.SaveChangesAsync();
+
+            return newEntry.Entity;
         }
+        #endregion
+
+        #region UpdateAsync
+        /// <summary>
+        /// Updates an existing entity in the database.
+        /// Attaches the provided entity to the context, marks it as modified, and saves changes to the database.
+        /// </summary>
+        /// <param name="entity">The entity to be updated.</param>
+        /// <returns>The updated entity after changes are saved to the database.</returns>
+        public async Task<TEntity?> UpdateAsync(TEntity entity)
+        {
+            using var context = _contextFactory.CreateDbContext();
+
+            context.Set<TEntity>().Attach(entity);
+            context.Entry(entity).State = EntityState.Modified;
+
+            await context.SaveChangesAsync();
+
+            return entity;
+        }
+        #endregion
+
+        #region SoftDeleteAsync
+        /// <summary>
+        /// Soft deletes an entity by setting an IsDeleted flag.
+        /// </summary>
+        /// <param name="id">The unique identifier of the entity to be soft-deleted.</param>
+        /// <returns>True if the soft delete is successful; otherwise, false.</returns>
+        public async Task<bool> SoftDeleteAsync(TKey id)
+        {
+            using var context = _contextFactory.CreateDbContext();
+
+            var entity = await context.Set<TEntity>().FindAsync(id);
+
+            if (entity is null) return false;
+
+            entity.IsDeleted = true;
+
+            return true;            
+        }
+        #endregion
+
+        #region HardDeleteAsync
+        /// <summary>
+        /// Hard deletes an entity from the database.
+        /// </summary>
+        /// <param name="id">The unique identifier of the entity to be hard-deleted.</param>
+        /// <returns>True if the hard delete is successful; otherwise, false.</returns>
+        public async Task<bool> HardDeleteAsync(TKey id)
+        {
+            using var context = _contextFactory.CreateDbContext();
+
+            var entity = await context.Set<TEntity>().FindAsync(id);
+
+            if (entity is null) return false;
+
+            context.Set<TEntity>().Remove(entity);
+
+            var rowsAffected = await context.SaveChangesAsync();
+
+            return rowsAffected > 0;
+        }
+        #endregion
+
+        #region RestoreAsync
+        /// <summary>
+        /// Restores a soft-deleted entity by setting the IsDeleted flag to false.
+        /// </summary>
+        /// <param name="id">The unique identifier of the entity to be restored.</param>
+        /// <returns>True if the restoration is successful; otherwise, false.</returns>
+        public async Task<bool> RestoreAsync(TKey id)
+        {
+            using var context = _contextFactory.CreateDbContext();
+
+            var entity = await context.Set<TEntity>().FindAsync(id);
+
+            if(entity is null) return false;
+
+            entity.IsDeleted = false;
+            return true;
+        }
+        #endregion
     }
 }
