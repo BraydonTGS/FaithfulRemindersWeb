fc8b61600913692043a5cd1a9867658af0803ba7
Braydon Sutherland <99298723+BraydonTGS@users.noreply.github.com>
2024-01-23 18:19:01 -0600

    Created User and TodoItem Repos

---
+++ FaithfulRemindersWeb.Business/Repository/ToDoItemRepository.cs
@@ -0,0 +1,14 @@
+﻿using FaithfulRemindersWeb.Business.Context;
+using FaithfulRemindersWeb.Business.Repository.Base;
+using FaithfulRemindersWeb.Entity.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FaithfulRemindersWeb.Business.Repository
+{
+    internal class ToDoItemRepository : BaseRepository<ToDoItem, Guid>
+    {
+        public ToDoItemRepository(IDbContextFactory<FaithfulDbContext> contextFactory) : base(contextFactory)
+        {
+        }
+    }
+}
---
+++ FaithfulRemindersWeb.Business/Repository/UserRepository.cs
@@ -0,0 +1,14 @@
+﻿using FaithfulRemindersWeb.Business.Context;
+using FaithfulRemindersWeb.Business.Repository.Base;
+using FaithfulRemindersWeb.Entity.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FaithfulRemindersWeb.Business.Repository
+{
+    internal class UserRepository : BaseRepository<User, Guid>
+    {
+        public UserRepository(IDbContextFactory<FaithfulDbContext> contextFactory) : base(contextFactory)
+        {
+        }
+    }
+}
