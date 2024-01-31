fd31154804077327874502d8c3d02b080cc54ee4
Braydon Sutherland <99298723+BraydonTGS@users.noreply.github.com>
2024-01-31 16:05:13 -0600

    Registered Business Logic Dependencies in Program.cs

--- FaithfulRemindersWeb.Api/Program.cs
+++ FaithfulRemindersWeb.Api/Program.cs
@@ -1,3 +1,5 @@
+using FaithfulRemindersWeb.Business.Config;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add Business Services //
+BusinessServices.ConfigureBusinessServices(builder.Services);
 
 var app = builder.Build();
 
