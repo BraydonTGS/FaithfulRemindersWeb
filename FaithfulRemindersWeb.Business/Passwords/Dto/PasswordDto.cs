e6b37079ba533c7d29fa347416162ae9b45c698d
Braydon Sutherland <99298723+BraydonTGS@users.noreply.github.com>
2024-02-06 19:54:52 -0600

    Created Password Dto

---
+++ FaithfulRemindersWeb.Business/Passwords/Dto/PasswordDto.cs
@@ -0,0 +1,23 @@
+﻿using FaithfulRemindersWeb.Business.Base;
+using FaithfulRemindersWeb.Entity.Entities;
+using System.Diagnostics;
+
+namespace FaithfulRemindersWeb.Business.Passwords.Dto
+{
+    [DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
+    internal class PasswordDto : BaseDto<Guid>
+    {
+        public byte[] Hash { get; set; } = Array.Empty<byte>();
+
+        public byte[] Salt { get; set; } = Array.Empty<byte>();
+
+        public Guid UserId { get; set; }
+
+        public User? User { get; set; }
+
+        private string GetDebuggerDisplay()
+        {
+            return $"Salt: {Salt} & Hash: {Hash}";
+        }
+    }
+}
