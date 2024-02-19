09d3b50b76616596829c45f63ec37bae0703457c
Braydon Sutherland <99298723+BraydonTGS@users.noreply.github.com>
2024-02-19 17:50:31 -0600

    Started to build out the Login and Registration Controllers

---
+++ FaithfulRemindersWeb.Api/Login/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using FaithfulRemindersWeb.Business.Login;
+using FaithfulRemindersWeb.Business.Users.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FaithfulRemindersWeb.Api.Login
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly ILoginBL _loginBL;
+
+        public LoginController(ILoginBL loginBL) => _loginBL = loginBL;
+
+        #region LoginSpecifiedUserAsync
+        /// <summary>
+        /// Attempt to Login the Specified User
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route(nameof(LoginSpecifiedUserAsync))]
+        public async Task<ActionResult<UserDto>> LoginSpecifiedUserAsync([FromQuery] UserDto user)
+        {
+            var result = await _loginBL.LoginUserAsync(user);
+
+            if (result == null) { return BadRequest(result); }
+
+            return Ok(result);
+        }
+        #endregion
+    }
+}
---
+++ FaithfulRemindersWeb.Api/Registration/RegistrationController.cs
@@ -0,0 +1,33 @@
+﻿using FaithfulRemindersWeb.Business.Registration;
+using FaithfulRemindersWeb.Business.Users.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FaithfulRemindersWeb.Api.Registration
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistrationController : ControllerBase
+    {
+        private readonly IRegistrationBL _registrationBL;
+
+        public RegistrationController(IRegistrationBL registrationBL) => _registrationBL = registrationBL;
+
+        #region
+        /// <summary>
+        /// Attempt to Register the Provided User Async
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<ActionResult<UserDto>> RegisterTheProvidedUserAsync(UserDto user)
+        {
+            var result = await _registrationBL.RegisterNewUserAsync(user);
+
+            if(result is null) { return BadRequest(result); }
+
+            return Ok(result);
+        }
+        #endregion
+    }
+}
